6d85cce3ac5caa943a9f0289fb16ee9b1991c1ad
GNPEREIRA <guspereira2010@gmail.com>
2021-04-29 14:19:47 -0300

    Versão final - corrigida

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let canvas = document.getElementById("snake")
-let context = canvas.getContext("2d")
+let canvas = document.getElementById("snake")//cria a cobra
+let context = canvas.getContext("2d")//cria o ambiente
 let box = 32
-let snake = []
+let snake = [] //cria a cobra como uma lista - série de coordenadas
 snake[0] = {
-    x: 8* box,
-    y: 8* box
+    x: 8 * box,
+    y: 8 * box
 }
 
 let direction = "right"
@@ -16,13 +16,13 @@ let food = {
 
 function criarBG(){
     context.fillStyle = "lightgreen"
-    context.fillRect(0,0,16 * box, 16 * box)
+    context.fillRect(0, 0, 16 * box, 16 * box)//desenho do retângulo (ambiente)
 }
 
 
 
 function criarCobrinha(){
-    for(i=0; i< snake.length; i++){
+    for(i=0; i < snake.length; i++){
         context.fillStyle = "green"
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
@@ -38,10 +38,10 @@ function drawFood(){
 document.addEventListener('keydown', update)
 
 function update(event){
-    if(event.keyCode == 37 && direction != "right") direction = "left"
-    if(event.keyCode == 38 && direction != "down") direction = "up"
-    if(event.keyCode == 39 && direction != "left") direction = "right"
-    if(event.keyCode == 40 && direction != "up") direction = "down"
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
     
 }
 
@@ -67,7 +67,7 @@ function iniciarJogo(){
     let snakeY = snake[0].y
 
     if(direction == "right") snakeX += box
-    if(direction == "left") snakeY -= box
+    if(direction == "left") snakeX -= box
     if(direction == "up") snakeY -= box
     if(direction == "down") snakeY += box
 
@@ -78,14 +78,12 @@ function iniciarJogo(){
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
 
-    snake.pop()
-
     let newHead = {
         x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(newHead)
+    snake.unshift(newHead)//unshift adiciona como 1º bloco da cobra
 }
 
 let jogo = setInterval(iniciarJogo, 100)

2398ca20d4fd007d5d30e885d46eab62bbabfba8
GNPEREIRA <guspereira2010@gmail.com>
2021-04-29 12:47:34 -0300

    manipulando elementos da cobrinha pt2

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ snake[0] = {
     y: 8* box
 }
 
+let direction = "right"
+
 function criarBG(){
     context.fillStyle = "lightgreen"
     context.fillRect(0,0,16 * box, 16 * box)
 }
+
+
 
 function criarCobrinha(){
     for(i=0; i< snake.length; i++){
@@ -19,5 +23,27 @@ function criarCobrinha(){
     }
 }
 
-criarBG()
-criarCobrinha()
+function iniciarJogo(){
+    criarBG()
+    criarCobrinha()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeY -= box
+    if(direction == "up") snakeY -= box
+    if(direction == "down") snakeY += box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100)
+
